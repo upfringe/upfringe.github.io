e94202f6ae5251fb580c01b09e5183fefdc0be8d
pakosaan <pyakurelutshab@gmail.com>
2024-08-28 09:36:14 +0545

    changes for build 8

--- next.config.js
+++ next.config.js
@@ -11,17 +11,27 @@ const nextConfig = {
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.externals.push({
-        bufferutil: "bufferutil",
-        "utf-8-validate": "utf-8-validate"
+        bufferutil: 'bufferutil',
+        'utf-8-validate': 'utf-8-validate',
       });
     }
     config.infrastructureLogging = {
-    level: "error",
+      level: 'error',
     };
 
+    // Add error handling for webpack configuration if needed
+    try {
+      // Additional Webpack configurations if needed
+    } catch (err) {
+      if (err instanceof Error) {
+        console.error('Webpack configuration error:', err.message);
+      } else {
+        console.error('An unknown error occurred during Webpack configuration.');
+      }
+    }
+
     return config;
-  }
+  },
 };
 
 module.exports = withContentlayer(nextConfig);
-
