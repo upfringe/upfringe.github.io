c4bc07e0c26bf795a491ff9a3ed1be50d572d335
pakosaan <pyakurelutshab@gmail.com>
2024-08-28 08:00:40 +0545

    changing the structure of next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,24 @@
-const { withContentlayer } = require('next-contentlayer')
+const { withContentlayer } = require('next-contentlayer');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output:'export',
-    images: {
-        unoptimized: true
-    },
-  compiler:{
-    removeConsole:true,
-  }
-}
-
-module.exports = withContentlayer(nextConfig,{webpack: (config, { isServer }) => {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  compiler: {
+    removeConsole: true,
+  },
+  webpack: (config, { isServer }) => {
     if (!isServer) {
-      config.externals.push({ bufferutil: "bufferutil", "utf-8-validate": "utf-8-validate"}); 
-      return config;
-    }}})
+      config.externals.push({
+        bufferutil: "bufferutil",
+        "utf-8-validate": "utf-8-validate"
+      });
+    }
+    return config;
+  }
+};
+
+module.exports = withContentlayer(nextConfig);
+
